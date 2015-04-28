cd336f4593c1b0472e4d7afc039169db746e8a25
junxier <ninjaolos@gmail.com>
2015-04-29 00:37:38 +0900

    Add PrintTest Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code herey
-        System.out.println("hello world!!");
+        //System.out.println("hello world!!");
+        PrintTest pt = new PrintTest();
+
+        pt.setStrSend("hello world!!");
+
+        System.out.println(pt.getStrSend());
     }
 }
---
+++ src/com/company/PrintTest.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Administrator on 2015-04-29.
+ */
+public class PrintTest {
+    private String strSend;
+
+    public String getStrSend() {
+        return strSend;
+    }
+
+    public void setStrSend(String strSend) {
+        this.strSend = strSend;
+    }
+}
